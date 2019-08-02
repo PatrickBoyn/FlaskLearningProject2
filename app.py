daac28f9312c9e072f5a053fcf2976f5ebb8980a
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-08-02 17:16:01 -0400

    Changed the name of the resource.

--- app.py
+++ app.py
@@ -26,6 +26,6 @@ class ItemList(Resource):
 
 
 api.add_resource(Item, '/item/<string:name>')
-api.add_resource(Items, '/items')
+api.add_resource(ItemList, '/items')
 
 app.run(port=5000, debug=True)
