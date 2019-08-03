0c320a8e99d19efcc9727dacfd4afc32a71b2f06
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-08-02 22:48:48 -0400

    Changed the userid mapping to set comprehension.

--- security.py
+++ security.py
@@ -4,11 +4,10 @@ users = [
     User(1, 'patrick', 'asdf')
 ]
 
+# Set comprehensions.
 username_mapping = {u.username: u for u in users}
 
-userid_mapping = {1:  {'id': 1,
-                       'username': 'patrick',
-                       'password': 'asdf'}}
+userid_mapping = {u.id: u for u in users}
 
 
 def authenticate(username, password):
