d15b256a6e66eefeb7a988d38ce5a4143ea04349
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-08-02 22:41:18 -0400

    Added an authentication, and identity method.

--- security.py
+++ security.py
@@ -13,3 +13,15 @@ username_mapping = {'patrick': {'id': 1,
 userid_mapping = {1:  {'id': 1,
                        'username': 'patrick',
                        'password': 'asdf'}}
+
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+
+    if user and user.password == password:
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_mapping.get(user_id)
