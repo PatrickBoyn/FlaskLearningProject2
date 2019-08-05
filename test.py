507c55d8192f832021f353f9613e46ba2762a604
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-08-05 16:54:56 -0400

    Created the first table.

---
+++ test.py
@@ -0,0 +1,9 @@
+# For learning how to use SQLite
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+create_table = 'CREATE TABLE users (id int, username text, password text)'
+cursor.execute(create_table)
