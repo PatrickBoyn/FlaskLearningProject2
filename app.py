23779f69ecaa7a0cd804d7a125e68c0d7d1a8d5e
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-08-04 21:44:45 -0400

    Added JWT authentication.

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
-app_secret_key = '132409875782345892345098723458902345987234583346=-01-1=--+65'
+app.secret_key = '132409875782345892345098723458902345987234583346'
 api = Api(app)
+
+# /auth
+jwt = JWT(app, authenticate, identity)
+
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
 
